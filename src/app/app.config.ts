e3d624fb9cf1783b13bd9443635d139ed90f1449
John Manuvel <sjohnmanuel04@gmail.com>
2025-07-14 11:21:08 +0530

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,29 +1,27 @@
-import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import {  provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { routes } from './app.routes';
 import { loaderInterceptor } from './interceptor/loader/loader-interceptor';
 import { httpInterceptor } from './interceptor/http/http-interceptor';
-import { provideAnimations } from '@angular/platform-browser/animations'; // ✅ Add this
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
-    provideToastr(),
-  provideHttpClient(
-    withInterceptors([loaderInterceptor])
-  ),
-  
-  provideHttpClient(
-    withInterceptors([httpInterceptor])
-  ),
-  provideAnimations(),
 
+    provideAnimations(),
+    provideToastr(),          
 
+    provideHttpClient(
+      withInterceptors([
+        loaderInterceptor,
+        httpInterceptor
+      ])
+    )
   ]
 };
