038ee583912c56ff2982e6dda427674b406b10fb
John Manuvel <sjohnmanuel04@gmail.com>
2025-07-16 18:18:39 +0530

    final

--- src/app/interceptor/http/http-interceptor.ts
+++ src/app/interceptor/http/http-interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
-  const backendUrl = 'http://localhost:3000';
+  const backendUrl = 'https://organic-backend-33fm.onrender.com';
 
   if (req.url.startsWith('http')) {
     return next(req);
